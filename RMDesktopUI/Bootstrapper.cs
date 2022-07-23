526a2aaab89212f479b932353b2cb2bc1d2cf103
Rishabh Wadhawan <rishabhwadhawan1996@gmail.com>
2022-07-23 16:05:11 +0530

    added dependency injection

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 using Caliburn.Micro;
 
@@ -8,9 +11,19 @@ namespace RMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container.Instance(container);
+            container.Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            GetType().Assembly.GetTypes().Where(type => type.IsClass).Where(type => type.Name.EndsWith("ViewModel")).ToList().ForEach(viewModelType => container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -18,5 +31,19 @@ namespace RMDesktopUI
             DisplayRootViewForAsync<ShellViewModel>();
         }
 
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
     }
 }
