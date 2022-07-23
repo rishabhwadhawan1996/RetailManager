49f2ee4a46c1fcbf6877e4d558ce568fb8b54ad9
Rishabh Wadhawan <rishabhwadhawan1996@gmail.com>
2022-07-23 15:30:39 +0530

    Adding UI project
    added ui project and caliburn micro

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+using Caliburn.Micro;
+
+using RMDesktopUI.ViewModels;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+    }
+}
